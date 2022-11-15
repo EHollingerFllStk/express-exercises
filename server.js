810cb4538d63bbff1b1b853ea415cea0659ce748
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-11-15 16:48:33 -0500

    Magic Ball Complete

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     res.send("Your tip is $" + req.params.total * (req.params.tipPercentage/100) + ".")
 })
 
+
+//Exercise 3 - Magic 8 Ball at use magic/random
+const magic = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+const random = magic[Math.floor(Math.random() * magic.length)]
+
+ app.get("/magic/:random", (req, res) => {
+    res.send(`Magic Ball says ${random}`)
+ })
+
 //Set up listener
 app.listen(3000, () => {
     console.log("Express is listening!")
